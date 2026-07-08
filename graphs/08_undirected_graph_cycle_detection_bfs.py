ea074aee539189fefbac7456d037e98eae9cd7a6
Shubhi Jain <jainshubhi67@gmail.com>
2026-07-08 15:37:42 +0530

    Add cycle detection method for undirected graph
    Implement cycle detection in an undirected graph using BFS.

---
+++ graphs/08_undirected_graph_cycle_detection_bfs.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isCyclic(self, V, edges):
+        adj = [[] for _ in range(V)]
+        indegree = [0] * V
+    
+        for source, dest in edges:
+            adj[source].append(dest)
+            indegree[dest] += 1
+    
+        queue = deque(i for i in range(V) if indegree[i] == 0)
+        topo = []
+    
+        while queue:
+            node = queue.popleft()
+            topo.append(node)
+            for neighbour in adj[node]:
+                indegree[neighbour] -= 1
+                if indegree[neighbour] == 0:
+                    queue.append(neighbour)
+    
+        return len(topo) != V
