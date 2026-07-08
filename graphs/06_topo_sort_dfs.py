ea831a0a8e1c2196b2d342ccbdb26a63ba8f418c
Shubhi Jain <shubhi.jain@grofers.com>
2026-07-08 14:56:47 +0530

    Add DFS-based topological sort implementation
    Implement topological sorting using DFS in a new class.

---
+++ graphs/06_topo_sort_dfs.py
@@ -0,0 +1,21 @@
+class Solution:
+    def topoSort(self, V, edges):
+        adj = [[] for _ in range(V)]
+        for source, dest in edges:
+            adj[source].append(dest)
+        visited = [False]*V
+        topo = []
+        for node in range(V):
+            if not visited[node]:
+                self._topo_dfs(adj, node, visited, topo)
+        topo.reverse()
+        return topo
+        
+    def _topo_dfs(self, adj, node, visited, topo):
+        if visited[node]:
+            return
+        visited[node] = True
+        for child in adj[node]:
+            self._topo_dfs(adj, child, visited, topo)
+        topo.append(node)
+        
