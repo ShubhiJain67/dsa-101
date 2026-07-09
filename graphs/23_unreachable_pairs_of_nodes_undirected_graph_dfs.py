ca41132b1ac595003111459431485eaf4fed996e
Shubhi Jain <shubhi.jain@grofers.com>
2026-07-09 15:25:32 +0530

    Implement DFS for counting unreachable node pairs

---
+++ graphs/23_unreachable_pairs_of_nodes_undirected_graph_dfs.py
@@ -0,0 +1,39 @@
+class Solution:
+    def countPairs(self, n: int, edges: List[List[int]]) -> int:
+        connectComponents = self.getConnectedComponentsDFS(n, edges)
+        count = 0
+        # TIME CONSUMING
+        # for i in range(len(connectComponents)):
+        #     for j in range(i+1, len(connectComponents)):
+        #         count += len(connectComponents[i])*len(connectComponents[j])
+        remaining = n
+        for component in connectComponents:
+            size = len(component)
+            remaining -= size
+            count += size * remaining
+        return count
+
+    def getAdjList(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        adj = [[] for _ in range(n)]
+        for node1, node2 in edges:
+            adj[node1].append(node2)
+            adj[node2].append(node1)
+        return adj
+
+    def getConnectedComponentsDFS(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        adj = self.getAdjList(n, edges)
+        visited = [False] * n
+        components = []
+        for i in range(n):
+            group = []
+            if not visited[i]:
+                self.dfs(adj, i, visited, group)
+            components.append(group)
+        return components  
+    
+    def dfs(self, adj, node, visited, group):
+        visited[node] = True
+        group.append(node)
+        for neigh in adj[node]:
+            if not visited[neigh]:
+                self.dfs(adj, neigh, visited, group)
