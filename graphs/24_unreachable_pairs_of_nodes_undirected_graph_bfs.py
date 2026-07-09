b0f4f4abc48c1d915280148bc129fa821eaadfa4
Shubhi Jain <shubhi.jain@grofers.com>
2026-07-09 15:41:56 +0530

    Add BFS solution for counting unreachable node pairs
    Implement BFS to count unreachable pairs of nodes in an undirected graph.

---
+++ graphs/24_unreachable_pairs_of_nodes_undirected_graph_bfs.py
@@ -0,0 +1,46 @@
+from collections import deque
+class Solution:
+    def countPairs(self, n: int, edges: List[List[int]]) -> int:
+        connectComponents = self.getConnectedComponentsBFS(n, edges)
+        print(connectComponents)
+        count = 0
+        # TIME CONSUMING
+        # for i in range(len(connectComponents)):
+        #     for j in range(i+1, len(connectComponents)):
+        #         count += len(connectComponents[i])*len(connectComponents[j])
+        remaining = n
+        for component in connectComponents:
+            size = len(component)
+            remaining -= size
+            count += size * remaining
+        return count
+
+    def getAdjList(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        adj = [[] for _ in range(n)]
+        for node1, node2 in edges:
+            adj[node1].append(node2)
+            adj[node2].append(node1)
+        return adj
+
+    def getConnectedComponentsBFS(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        adj = self.getAdjList(n, edges)
+        visited = [False] * n
+        components = []
+        for i in range(n):
+            group = []
+            if not visited[i]:
+                self.bfs(adj, i, visited, group)
+                components.append(group)
+        return components 
+
+    def bfs(self, adj, node, visited, group):
+        que = deque([node])
+        while len(que) > 0:
+            node = que.popleft()
+            if visited[node]:
+                continue
+            group.append(node)
+            visited[node] = True
+            for neigh in adj[node]:
+                if not visited[neigh]:
+                    que.append(neigh)
