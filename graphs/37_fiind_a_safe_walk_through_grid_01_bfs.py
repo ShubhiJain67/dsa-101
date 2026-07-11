924883a0ac336ac25a608f5dfb97a20086008ed4
Shubhi Jain <jainshubhi67@gmail.com>
2026-07-11 13:41:03 +0530

    Implement BFS for safe walk through grid
    Refactor findSafeWalk to use BFS and remove unused methods.

---
+++ graphs/37_fiind_a_safe_walk_through_grid_01_bfs.py
@@ -0,0 +1,26 @@
+class Solution:
+    directions = [[-1,0],[1,0],[0,-1],[0,1]]
+    def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
+        return self.walk01BFS(grid, health, 0, 0, len(grid)-1, len(grid[0])-1)
+  
+    def walk01BFS(self, grid: List[List[int]], health: int, sourceI: int, sourceJ: int, targetI: int, targetJ: int) -> bool:
+        que = deque()
+        bestHealth = [[math.inf]*(len(grid[0])) for _ in range(len(grid))]
+        que.append((grid[sourceI][sourceJ], (sourceI,sourceJ)))
+        bestHealth[sourceI][sourceJ] = grid[sourceI][sourceJ]
+        while que:
+            currHealth, (currI, currJ) = que.popleft()
+            if currHealth > bestHealth[currI][currJ]:
+                continue
+            for di, dj in self.directions:
+                ni = currI + di
+                nj = currJ + dj
+                if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]):
+                    newHealth = currHealth + grid[ni][nj]
+                    if newHealth < bestHealth[ni][nj]:
+                        bestHealth[ni][nj] = newHealth
+                        if newHealth > 0:
+                            que.append((bestHealth[ni][nj], (ni, nj)))
+                        else:
+                            que.appendleft((bestHealth[ni][nj], (ni, nj)))
+        return bestHealth[targetI][targetJ] < health
