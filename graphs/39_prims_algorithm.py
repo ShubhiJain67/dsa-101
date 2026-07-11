befab38d4a289e4476dcb77a32662ca9dd5eda51
Shubhi Jain <jainshubhi67@gmail.com>
2026-07-11 19:45:45 +0530

    Add Prim's algorithm for minimum spanning tree
    Implement Prim's algorithm to find the minimum spanning tree.

---
+++ graphs/39_prims_algorithm.py
@@ -0,0 +1,25 @@
+import heapq
+class Solution:
+    def spanningTree(self, V, edges):
+        adjList = [[] for _ in range(V)]
+        for start, end, weight in edges:
+            adjList[start].append([end, weight])
+            adjList[end].append([start, weight])
+        # code here
+        minHeap = []
+        heapq.heappush(minHeap, (0, 0, -1))
+        vertextVisited = [False]*V
+        edgeCount = V
+        sumWeight = 0
+        while edgeCount>0:
+            weight, end, start = heapq.heappop(minHeap)
+            if vertextVisited[end]:
+                continue
+            sumWeight += weight
+            edgeCount -= 1
+            vertextVisited[end] = True
+            for n, w in adjList[end]:
+                if not vertextVisited[n]:
+                    heapq.heappush(minHeap, (w, n, end))
+                    
+        return sumWeight
