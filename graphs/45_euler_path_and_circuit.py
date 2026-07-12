e0798801a98e17d1ef2471b5495eaf600a525197
Shubhi Jain <jainshubhi67@gmail.com>
2026-07-12 14:07:07 +0530

    Implement Euler path and circuit detection

---
+++ graphs/45_euler_path_and_circuit.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isEulerCircuitExist(self, V, adj):
+        degrees = [len(adj[i]) for i in range(V)]
+
+        start = -1
+        for i in range(V):
+            if degrees[i] > 0:
+                start = i
+                break
+        if start == -1:
+            return 2
+        visited = [False] * V
+        self.dfs(start, visited)
+
+        for i in range(V):
+            if degrees[i] > 0 and not visited[i]:
+                return 0
+
+        odd = 0
+        for degree in degrees:
+            if degree % 2 == 1:
+                odd += 1
+
+        if odd == 0:
+            return 2
+        elif odd == 2:
+            return 1
+        else:
+            return 0
+    
+    def dfs(self, node, visited):
+        visited[node] = True
+        for nei in adj[node]:
+            if not visited[nei]:
+                self.dfs(nei, visited)
