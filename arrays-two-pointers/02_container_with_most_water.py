044c33d3c927cfbddbcb947292b144c3ccc50b59
Shubhi Jain <jainshubhi67@gmail.com>
2026-07-22 23:18:12 +0530

    Implement maxArea function for container with most water

---
+++ arrays-two-pointers/02_container_with_most_water.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        p1 = 0
+        p2 = len(height) - 1
+        maxA = 0
+        while p1 < p2:
+            currArea = min(height[p1], height[p2])* (p2-p1)
+            if maxA < currArea:
+                maxA = currArea
+            if height[p1] < height[p2]:
+                p1+=1
+            else:
+                p2-=1
+        return maxA
