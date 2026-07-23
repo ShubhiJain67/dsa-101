3747f54d037478cbeaa1473ad04cec94a5338a0a
Shubhi Jain <jainshubhi67@gmail.com>
2026-07-23 12:22:47 +0530

    Fix zeroes moving logic in move_zeroes.py

---
+++ arrays-two-pointers/09_move_zeroes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        p1 = 0
+        p2 = 0
+        
+        while p2 < len(nums):
+            while p2 < len(nums) and nums[p2] == 0:
+                p2 += 1
+            if p2 == len(nums):
+                return
+            temp = nums[p1]
+            nums[p1] = nums[p2]
+            nums[p2] = temp
+            p1 += 1
+            p2 += 1
