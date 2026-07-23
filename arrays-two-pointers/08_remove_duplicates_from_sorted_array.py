f2489c3fea30a29e6c848f01e459e34a293655c5
Shubhi Jain <jainshubhi67@gmail.com>
2026-07-23 11:14:03 +0530

    Add removeDuplicates function in Solution class
    Implement a solution to remove duplicates from a sorted array in Python.

---
+++ arrays-two-pointers/08_remove_duplicates_from_sorted_array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        p1 = 0
+        p2 = 1
+        count = 1
+        while p2 < len(nums):
+            while p2 < len(nums) and nums[p1] == nums[p2]:
+                p2 += 1
+            if p2 == len(nums):
+                return count
+            p1 += 1
+            temp = nums[p1]
+            nums[p1] = nums[p2]
+            nums[p2] = temp
+            count += 1
+            p2 += 1
+            # print(nums)
+        return count
+            
+        
