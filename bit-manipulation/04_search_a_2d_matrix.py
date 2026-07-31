e14cef362c68161ebfeb7411266fcc2d9ca9b575
Shubhi Jain <jainshubhi67@gmail.com>
2026-07-31 09:47:02 +0530

    Implement searchMatrix for 2D matrix search

---
+++ bit-manipulation/04_search_a_2d_matrix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        low = 0
+        high = len(matrix)*len(matrix[0]) - 1
+        while low <= high:
+            mid = low + (high-low)//2
+            x, y = self.getCoordinates(mid, matrix)
+            if matrix[x][y] == target:
+                return True
+            elif matrix[x][y] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return False
+    
+    def getCoordinates(self, index, matrix):
+        r = len(matrix)
+        c = len(matrix[0])
+        x = int(index/c)
+        y = index - x*c
+        return x, y
