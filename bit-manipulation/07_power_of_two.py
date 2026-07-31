cd89041804bfcb4a8d4b750bef8e0fe58a9288fe
Shubhi Jain <jainshubhi67@gmail.com>
2026-07-31 12:45:38 +0530

    Optimize power of two check with bit manipulation
    Refactor isPowerOfTwo method to use bit manipulation for efficiency.

--- bit-manipulation/07_power_of_two.py
+++ bit-manipulation/07_power_of_two.py
@@ -1,5 +1,14 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
+        if n == 0:
+            return False
+        # return self.isPowerOfTwoBruteForce(n)
+        return self.isPowerOfTwoBitManipulation(n)
+    
+    def isPowerOfTwoBitManipulation(self, n):
+        return (n & (n-1) == 0)
+    
+    def isPowerOfTwoBruteForce(self, n):
         count = 0
         while n > 0:
             if n % 2 == 1:
