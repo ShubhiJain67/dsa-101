29929036d21ce795dcc9714be2ac3d523ce0c91e
Shubhi Jain <jainshubhi67@gmail.com>
2026-08-01 12:24:13 +0530

    Add generateParenthesis solution with three methods
    Implement a solution for generating valid parentheses combinations using recursion, dynamic programming, and iteration.

---
+++ backtracking/12_generate_parentheses.py
@@ -0,0 +1,45 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # return list(self.getParRec(n))
+        # return list(self.getParDP(n))
+        return list(self.getParIter(n))
+    
+    def getParRec(self, n):
+        if n == 0:
+            return set([""])
+        combinations = set()
+        restCombinations = self.getParRec(n-1)
+        for comb in restCombinations:
+            for i in range(len(comb)+1):
+                newPar = comb[:i]+"()"+comb[i:]
+                combinations.add(newPar)
+        return combinations
+    
+    def getParDP(self, N):
+        dp = [None]*(N+1)
+        for n in range(N+1):
+            if n == 0:
+                combinations = set([""])
+            else:
+                combinations = set()
+                restCombinations = dp[n-1]
+                for comb in restCombinations:
+                    for i in range(len(comb)+1):
+                        newPar = comb[:i]+"()"+comb[i:]
+                        combinations.add(newPar)
+            dp[n] = combinations
+        return dp[N]
+    
+    def getParIter(self, N):
+        totalCombinations = []
+        for n in range(N+1):
+            if n == 0:
+                combinations = set([""])
+            else:
+                combinations = set()
+                for comb in totalCombinations:
+                    for i in range(len(comb)+1):
+                        newPar = comb[:i]+"()"+comb[i:]
+                        combinations.add(newPar)
+            totalCombinations = combinations
+        return totalCombinations
