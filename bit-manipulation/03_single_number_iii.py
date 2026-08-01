fffa68acb9491a36496c6a791b3dfd02a8fcc570
Shubhi Jain <jainshubhi67@gmail.com>
2026-08-01 13:49:31 +0530

    Implement singleNumber method to find unique numbers

---
+++ bit-manipulation/03_single_number_iii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        allTogether = 0
+        for num in nums:
+            allTogether = allTogether ^ num
+        firstSetBit = allTogether & -allTogether
+        firstNum = 0
+        for num in nums:
+            if num & firstSetBit:
+                firstNum = firstNum ^ num
+        secondNum = allTogether ^ firstNum
+        return [firstNum, secondNum]
