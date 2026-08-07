ac9abb315948dec37380390e5026826c499c729d
Shubhi Jain <jainshubhi67@gmail.com>
2026-08-07 10:01:11 +0530

    Add function to find maximum average subarray
    Implement sliding window technique to find the maximum average of subarrays of size k.

---
+++ sliding-window/01_maximum_average_subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        currSum = 0
+        for i in range(k):
+            currSum += nums[i]
+        maxAvg = currSum
+        for i in range(k, len(nums)):
+            currSum = currSum + nums[i] - nums[i-k]
+            if maxAvg < currSum:
+                maxAvg = currSum
+
+        return maxAvg / k
