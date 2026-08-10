7896069973cd45ab9a44c4a97fbe290932a0cb7b
Shubhi Jain <jainshubhi67@gmail.com>
2026-08-10 12:01:56 +0530

    Implement water trapping algorithm in Python

---
+++ stacks/07_trapping_rain_water_stack.py
@@ -0,0 +1,49 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # return self.trapIntiution(height)
+        return self.trapOptimised(height)
+
+
+    def trapOptimised(self, height):
+        maxRight = self.getMaxRight(height)
+        index = 0
+        maxSeen = 0
+        water = 0
+        while index < len(height):
+            water += max(0, min(maxRight[index], maxSeen) - height[index])
+            if height[index] > maxSeen:
+                maxSeen = height[index]
+            index += 1
+        return water
+
+    def trapIntiution(self, height):
+        maxRight = self.getMaxRight(height)
+        maxLeft = self.getMaxLeft(height)
+        water = 0
+        index = 0
+        while index < len(height):
+            water += max(0, min(maxRight[index], maxLeft[index]) - height[index])
+            index += 1
+        return water
+
+    def getMaxRight(self, height):
+        maxRight = [0]*len(height)
+        index = len(height) - 1
+        maxSeen = 0
+        while index >= 0:
+            maxRight[index] = maxSeen
+            if height[index] > maxSeen:
+                maxSeen = height[index]
+            index -= 1
+        return maxRight
+
+    def getMaxLeft(self, height):
+        maxLeft = [0]*len(height)
+        index = 0
+        maxSeen = 0
+        while index < len(height):
+            maxLeft[index] = maxSeen
+            if height[index] > maxSeen:
+                maxSeen = height[index]
+            index += 1
+        return maxLeft
