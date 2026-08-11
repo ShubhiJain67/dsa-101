bd8a0a1ad8ce365c003b0561ef8b1a7229d464d9
Shubhi Jain <jainshubhi67@gmail.com>
2026-08-11 18:58:19 +0530

    Implement optimized merging of k sorted linked lists

---
+++ heaps/06_merge_k_sorted_lists_heap.py
@@ -0,0 +1,132 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # return self.merge(lists, 0, len(lists)-1)
+        # return self.mergeHeaps(lists)
+        return self.mergeHeapsOptimised(lists)
+
+    
+    def mergeHeapsOptimised(self, lists):
+        heap = []
+        index = 0
+        for lst in lists:
+            if lst is None:
+                continue
+            heap.append([lst.val, index, lst])
+            index += 1
+        heapq.heapify(heap)
+        head = None
+        ptr = None
+        while heap:
+            value, index, curr = heapq.heappop(heap)
+            if head == None:
+                head = curr
+                ptr = curr
+            else:
+                ptr.next = curr
+                ptr = ptr.next
+            if curr.next:
+                heapq.heappush(heap, [curr.next.val, index, curr.next])
+        return head
+
+
+    def mergeHeaps(self, lists):
+        heap = []
+        for index in range(len(lists)):
+            l = lists[index]
+            count = 0
+            while l:
+                curr = l
+                l = l.next
+                curr.next = None
+                heap.append([curr.val, index, count, curr])
+                count += 1
+        heapq.heapify(heap)
+        head = None
+        ptr = None
+        while heap:
+            _, _, _, curr = heapq.heappop(heap)
+            if head == None:
+                head = curr
+                ptr = curr
+            else:
+                ptr.next = curr
+                ptr = ptr.next
+        return head
+
+    def merge(self, lists, start, end):
+        if not lists or start > end:
+            return None
+        if start == end:
+            return lists[start]
+        listLen = start + (end - start)//2
+        leftMerge = self.merge(lists, start, listLen)
+        rightMerge = self.merge(lists, listLen+1, end)
+        # return self.merge2SortedListsOutPlace(leftMerge, rightMerge)
+        # return self.merge2SortedListsInPlace(leftMerge, rightMerge)
+        return self.merge2Optimised(leftMerge, rightMerge)
+    
+    def merge2Optimised(self, list1, list2):
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        if list1.val < list2.val:
+            list1.next = self.merge2Optimised(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.merge2Optimised(list1, list2.next)
+            return list2
+
+    def merge2SortedListsInPlace(self, list1, list2):
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        if list1.val > list2.val:
+            list1, list2 = list2, list1
+        head = list1
+        while list1 and list2:
+            prev = None
+            while list1 and list1.val <= list2.val:
+                prev = list1
+                list1 = list1.next
+            prev.next = list2
+            list1, list2 = list2, list1
+
+        return head
+        
+        
+    def merge2SortedListsOutPlace(self, list1, list2):
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        p1 = list1
+        p2 = list2
+        p3 = None
+        newHead = None
+        while p1 and p2:
+            if p1.val < p2.val:
+                newNode = ListNode(p1.val, None)
+                p1 = p1.next
+            else:
+                newNode = ListNode(p2.val, None)
+                p2 = p2.next
+            if not newHead:
+                newHead = newNode
+            if not p3:
+                p3 = newNode
+            else:
+                p3.next = newNode
+                p3 = p3.next
+        if p1:
+            p3.next = p1
+        if p2:
+            p3.next = p2
+        return newHead
+        
